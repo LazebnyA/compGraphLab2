0124d9baf08e2701a5a468552bbe6302af52e322
Oleksandr Lazebny <oleksandrlazebnij8@gmail.com>
2022-11-20 03:53:45 +0200

    first commit

---
+++ lab2.py
@@ -0,0 +1,19 @@
+from PIL import Image, ImageDraw
+
+with open('C:\compGraphics\compGraphLab2\DS4.txt', 'r') as file:  # в першому аргументі потрібно змінити шлях, на відповідний шлях до файлу DS4.txt
+    image = Image.new("RGBA", (960, 540), 'white')    
+
+    draw = ImageDraw.Draw(image)
+
+    for line in file:
+        coordinatesTuple = (int(line[0:3]), int(line[4:7]))
+        draw.point(coordinatesTuple, (0, 0, 0))
+
+del draw
+image.save("C:\compGraphics\compGraphLab2\img.png", "PNG")  # в першому аргументі потрібно змінити/поставити шлях, по якому ви хочете зберегти зображення і відповідно назву файлу
+        
+
+
+
+
+
